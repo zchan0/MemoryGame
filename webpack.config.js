2d6bdf1a99581b69077f0236e5f3e9fd305fc11c
CC <zchan0@outlook.com>
2017-11-19 13:06:22 -0500

    Webpack & Babel

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+var path = require('path');
+
+module.exports = {
+    entry: './js/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }]
+    }
+};
